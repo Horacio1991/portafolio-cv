182450b39701406750879fb0656e9e70a81639bb
Horacio1991 <horacio0291@gmail.com>
2024-05-12 11:34:13 -0300

    Subo Sitio

---
+++ script.js
@@ -0,0 +1,78 @@
+/* ********** Menu ********** */
+((d) => {
+  const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu");
+
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+})(document);
+
+/************ Carrusel ************** */
+var slideIndex = 1;
+mostrarSlides(slideIndex);
+
+function cambiarSlide(n) {
+    mostrarSlides(slideIndex += n);
+}
+
+function mostrarSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("carrusel-slide");
+    var descriptions = document.getElementsByClassName("description");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+        descriptions[i].style.display = "none";  // Asegura que las descripciones también se oculten
+    }
+    slides[slideIndex-1].style.display = "block";  
+    descriptions[slideIndex-1].style.display = "block"; // Muestra la descripción asociada
+}
+
+
+/* ********** ContactForm ********** */
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/horacio0291@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias"; //Modifico la URL con #gracias para que active la vetnana modal
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta nuevamente"; //Si no existe status text va el mensaje personalizado
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`; 
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
+})(document);
